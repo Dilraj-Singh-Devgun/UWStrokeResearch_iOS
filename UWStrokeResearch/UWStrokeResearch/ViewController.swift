c0d356749c102a342592cd2e75264974f7993b0c
Dilraj Devgun <Dilraj@Dilrajs-MacBook-Pro.local>
2016-11-27 15:29:57 -0800

    initial commit

---
+++ UWStrokeResearch/UWStrokeResearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UWStrokeResearch
+//
+//  Created by Dilraj Devgun on 11/27/16.
+//  Copyright © 2016 Dilraj Devgun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
